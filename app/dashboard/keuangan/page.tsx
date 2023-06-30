38969417ebbee7a39c6ff950039f199d88dc3716
luqmanamirullah <amirullahluqman@gmail.com>
2023-07-01 02:24:00 +0700

    feat: :bricks: side bar and code infrastuktur
    this commit, allows you to create after sign in page with nice looking code
    
    side bar , header after sign page, route struktur + responsive

---
+++ app/dashboard/keuangan/page.tsx
@@ -0,0 +1,18 @@
+export default function page() {
+  return (
+    <>
+      {/* Route Section */}
+      <section>
+        <ul className="flex gap-4 items-center route">
+          <li>
+            <h1 className="text-xs md:text-lg font-semibold md:font-bold">
+              Keuangan
+            </h1>
+          </li>
+        </ul>
+      </section>
+      {/* Another Content */}
+      <section></section>
+    </>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,18 @@
+'use client';
+import { usePathname } from 'next/navigation';
 import Navbar from '@/components/Navbar';
 import './globals.css';
-import { Montserrat } from 'next/font/google';
+import { Montserrat, Inter } from 'next/font/google';
 import Footer from '@/components/Footer';
+import Sidebar from '@/components/dashboard/Sidebar';
 
 const montserrat = Montserrat({ subsets: ['latin'] });
 
+const inter = Inter({ subsets: ['latin'] });
+
 export const metadata = {
   title: 'ORBIT',
-  description: 'Organization of Basic Information Technology ',
+  description: 'Organization of Basic Information Technology',
 };
 
 export default function RootLayout({
@@ -15,6 +20,21 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const pathname = usePathname();
+  const isDashboard =
+    pathname === '/dashboard' || pathname.startsWith('/dashboard/');
+
+  if (isDashboard) {
+    return (
+      <html lang="en">
+        <body className={`${inter.className} bg-[#1C2039]`}>
+          <Sidebar />
+          {children}
+        </body>
+      </html>
+    );
+  }
+
   return (
     <html lang="en">
       <body className={`${montserrat.className}`}>
