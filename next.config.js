f30795fe3efb61d5b56bf84fa754581e32b3295b
luqmanamirullah <amirullahluqman@gmail.com>
2023-07-10 10:14:55 +0700

    ci: add image domains (still localhost)

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 // const nextConfig = {}
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['localhost'],
+  },
+};
 
 module.exports = nextConfig;
