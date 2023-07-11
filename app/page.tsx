fff5a43fc6957b961e9073ee88b3d20b6dd26aae
luqmanamirullah <amirullahluqman@gmail.com>
2023-07-11 12:14:06 +0700

    refactor: :art:

--- app/page.tsx
+++ app/page.tsx
@@ -135,8 +135,6 @@ export default function Home() {
       handleActive('divisions');
     }
   }, [inViewHome, inViewAbout, inViewDivisions]);
-  console.log(inViewHome, inViewAbout, inViewDivisions);
-
   const cardDeftRef = useRef(null);
   const cardActvRef = useRef(null);
 
