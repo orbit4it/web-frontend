c4f9d07dffe2e41ba780e230421b266d44a56526
luqmanamirullah <amirullahluqman@gmail.com>
2023-07-11 12:13:28 +0700

    refactor: :art:

--- components/landingPage/Person.tsx
+++ components/landingPage/Person.tsx
@@ -8,7 +8,7 @@ const Person: React.FC<StrukturProps> = (props) => {
   const [onHover, setOnHover] = useState(false);
   return (
     <div
-      className="relative grid justify-center w-64 h-full cursor-pointer"
+      className="relative grid justify-center w-1/2 md:w-1/3 h-full cursor-pointer"
       onMouseEnter={() => setOnHover(true)}
       onMouseLeave={() => setOnHover(false)}
     >
@@ -18,10 +18,12 @@ const Person: React.FC<StrukturProps> = (props) => {
             initial={{ opacity: 0, x: -100, y: 100 }}
             animate={{ opacity: 1, x: 0, y: 0 }}
             transition={{ duration: 0.2, type: 'tween' }}
-            className="absolute top-4 -right-36 w-48 h-20 px-2 py-1"
+            className="absolute top-7 md:top-3 -right-2 md:-right-36 w-28 h-12 md:w-48 md:h-20  md:px-1 py-1 z-10 flex flex-col justify-center"
           >
-            <h1 className="text-sm text-white">Hai! Saya {props.name}</h1>
-            <p className="text-xs text-white/60">
+            <h1 className="text-[0.5rem] w-full text-center md:text-start md:text-xs text-white">
+              Hai! Saya {props.fullName}
+            </h1>
+            <p className="text-[0.5rem] md:text-xs text-white/60 hidden md:block">
               Senang berkenalan dengan anda
             </p>
           </motion.div>
@@ -29,7 +31,7 @@ const Person: React.FC<StrukturProps> = (props) => {
             initial={{ opacity: 0, x: -100, y: 100 }}
             animate={{ opacity: 1, x: 0, y: 0 }}
             transition={{ duration: 0.2, type: 'tween' }}
-            className="absolute top-0 -right-40 w-60 z-10 "
+            className="absolute top-5 md:top-0 -right-5 md:-right-40 w-36 md:w-60 z-10 "
           >
             <svg
               width="auto"
@@ -125,7 +127,7 @@ const Person: React.FC<StrukturProps> = (props) => {
         className="relative flex items-center justify-center"
       >
         <div
-          className="h-full w-48 z-10 absolute"
+          className="w-28 h-4/5 md:w-48 z-10 absolute bottom-0"
           style={{
             borderRadius: '0px 0px 10px 10px',
             background:
@@ -140,25 +142,27 @@ const Person: React.FC<StrukturProps> = (props) => {
             scaleY: onHover ? 0.7 : 1,
           }}
           transition={{ duration: 1, type: 'tween' }}
-          className={twMerge(`w-48 h-52 ${props.imgHeight} relative`)}
+          className={twMerge(
+            `w-28 md:${props.imgWidth} h-40 md:h-64 md:${props.imgHeight} relative`
+          )}
         >
           <Image
             src={props.image}
             width={500}
             height={500}
-            className={`w-full h-auto absolute bottom-0 ${
-              onHover ? 'translate-y-10 ' : 'translate-y-36'
+            className={`w-28 h-40 md:w-full md:h-auto absolute bottom-0 left-1/2 -translate-x-1/2 ${
+              onHover ? 'translate-y-10 ' : 'translate-y-16 md:translate-y-36'
             } transition-all duration-300 ease-in-out`}
             alt="elite"
             priority
           />
         </motion.div>
-        <h3 className="font-bold text-3xl text-white text-center w-40 absolute mx-auto left-0 right-0 -bottom-4 z-20">
+        <h3 className="font-bold text-xl md:text-3xl text-white text-center w-40 absolute mx-auto left-1/2 -translate-x-1/2 right-0 -bottom-4 z-20">
           {props.name}
         </h3>
       </motion.div>
 
-      <p className="text-lg text-white/70 text-center mt-4 w-full z-20">
+      <p className="text-sm md:text-lg text-white/70 text-center mt-4 w-full z-20">
         {props.jabatan}
       </p>
     </div>
--- helper/interfaces.ts
+++ helper/interfaces.ts
@@ -78,6 +78,7 @@ export interface KelasProps {
 export interface StrukturProps {
   image: string;
   name: string;
+  fullName: string;
   jabatan: string;
   imgWidth: string;
   imgHeight: string;
