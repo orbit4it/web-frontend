3607ccbdfcc74d025737415d2c6b3baf00edf55f
luqmanamirullah <amirullahluqman@gmail.com>
2023-07-19 18:02:03 +0700

    feat: :bricks: add handling error and loading

---
+++ app/dashboard/loading.tsx
@@ -0,0 +1,14 @@
+export default function Loading() {
+  return (
+    <main className="w-full h-screen flex flex-col items-center justify-center">
+      <div className="loading">
+        <div className="circle"></div>
+        <div className="circle"></div>
+        <div className="circle"></div>
+        <div className="shadow"></div>
+        <div className="shadow"></div>
+        <div className="shadow"></div>
+      </div>
+    </main>
+  );
+}
