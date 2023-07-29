be319430c58305ee81ec06fc2727b29686259e3b
luqmanamirullah <amirullahluqman@gmail.com>
2023-07-29 23:16:59 +0700

    feat: :lock: add middleware based on user_token
    BREAKING CHANGE: should based on client side auth for maximal performance in implement jwt token

--- middleware.ts
+++ middleware.ts
@@ -1,46 +1,38 @@
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
+export const queryMe = `query {
+  me {
+    id
+  }      
+}`;
 
-export function middleware(req: NextRequest, res: NextResponse) {
+export function middleware(req: NextRequest) {
+  const token = req.cookies.get('user_token');
   if (
     process.env.DASHBOARD_STATUS == 'development' &&
     req.nextUrl.pathname.startsWith('/dashboard')
   ) {
     return NextResponse.redirect(new URL('/underconstruction', req.url));
-  } else if (
-    process.env.DASHBOARD_STATUS == 'development' &&
-    req.nextUrl.pathname.startsWith('/admin')
-  ) {
-    return NextResponse.redirect(new URL('/underconstruction', req.url));
   }
-  //   const checkCookie = async () => {
-  //     const res = await Apicall(`
-  //         {
-  //            me {
-  //             role
-  //           }
-  //         }
-  //     `);
-  //     if (res) {
-  //       if (res.data.me.role == 'admin') {
-  //         return NextResponse.redirect(new URL('/dashboard/admin', req.url));
-  //       } else {
-  //         return NextResponse.redirect(new URL('/dashboard', req.url));
-  //       }
-  //     } else {
-  //       return NextResponse.redirect(new URL('/login', req.url));
-  //     }
-  //   };
-  //   let cookie = req.cookies;
-  //   console.log('hasil: ' + req.url);
-  //   console.log(cookie);
-  //   return checkCookie();
+
+  if (req.nextUrl.pathname.startsWith('/admin/dashboard') && !token) {
+    return NextResponse.redirect(new URL('/login', req.url));
+  }
+
+  if (req.nextUrl.pathname.startsWith('/admin/:path*') && !token) {
+    return NextResponse.redirect(new URL('/login', req.url));
+  }
+
+  if (req.nextUrl.pathname.startsWith('/login') && token) {
+    return NextResponse.redirect(new URL('/admin/dashboard', req.url));
+  }
 }
-
 export const config = {
-  mather: ['/dashboard', '/admin'],
+  matcher: [
+    '/dashboard',
+    '/admin/dashboard',
+    '/login',
+    '/admin/:path*',
+    '/dashboard/:path*',
+  ],
 };
-
-// export const config = {
-//   matcher: ['/', '/register', '/login', '/dashboard', '/dashboard/admin'],
-// };
